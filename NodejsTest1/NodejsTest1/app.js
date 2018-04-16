d43c3fd880a6d9351ca122857f493651885ed60f
reuvenshenkin <reuven.sheinkin@capriza.com>
2018-04-16 14:52:33 +0300

    initial commit

---
+++ NodejsTest1/NodejsTest1/app.js
@@ -0,0 +1,25 @@
+﻿
+const request = require('superagent')
+
+
+function getPosts()
+{
+    request('https://jsonplaceholder.typicode.com/posts/')
+        .then(response => response.text)
+        .then(text => console.log(text))
+}
+
+function getPost(postId)
+{
+    var url = 'https://jsonplaceholder.typicode.com/posts/' + postId + '/comments'
+    request(url)
+        .then(response => response.text)
+        .then(text => console.log(text))
+}
+
+
+if (process.argv[2])
+    getPost(process.argv[2])
+
+else
+    getPosts()
